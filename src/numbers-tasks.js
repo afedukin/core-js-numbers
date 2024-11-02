653832e1e1eed81555e1cf728374d0ef8a19d099
afedukin <fedukin.aa@yandex.ru>
2024-11-02 20:40:11 +0400

    feat: add solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -253,8 +253,11 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const fib = [
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
+  ];
+  return fib[index];
 }
 
 /**
@@ -268,8 +271,10 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i += 1) sum += i;
+  return sum;
 }
 
 /**
